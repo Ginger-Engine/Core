39d99d11cd40caabaa99ffe43dba4263db9036bd
aleksey.pilov <aleksey.pilov@yandex.ru>
2025-06-04 11:25:46 +0300

    initial

--- obj/Debug/net9.0/Engine.Core.AssemblyInfo.cs
+++ obj/Debug/net9.0/Engine.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96e62d166645232d0a3bb6750885b85c3d40a0e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
